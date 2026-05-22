5c9df5579f70872c289096834a2df73d2066c7fa
Franco Ramonda <framonda@siapcn.it>
2026-05-22 10:10:42 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.frenky.egypt"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "2.3.2"
+        versionCode = 28
+        versionName = "2.3.3"
 
         buildConfigField("String", "MAPS_API_KEY", "\"$mapsApiKey\"")
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
